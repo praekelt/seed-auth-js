ed5b9516a6a6c780f3f34848efdecf266a4bf04f
justinvdm <justinvdm.0.1.0@gmail.com>
2016-05-23 12:09:02 +0200

    Change http.request() to allow query params to be given directly in options object

--- tests/request.test.js
+++ tests/request.test.js
@@ -6,7 +6,7 @@ const { fail } = expect;
 
 describe("auth.request", () => {
   it("should prepend the prefix to requests", () => {
-    return auth.request({prefix: 'http://localhost:8000'}, {
+    return auth.request({conf: {prefix: 'http://localhost:8001'}}, {
         method: 'GET',
         url: '/organizations/'
       })
@@ -14,7 +14,7 @@ describe("auth.request", () => {
   });
 
   it("should throw error responses", () => {
-    return auth.request({prefix: 'http://localhost:8000'}, {
+    return auth.request(null, {
         method: 'GET',
         url: '/i-do-not-exist'
       })
@@ -25,11 +25,26 @@ describe("auth.request", () => {
   });
 
   it("should support token auth", () => {
-    return auth.request({token: '1234'}, {
+    return auth.request({conf: {token: '1234'}}, {
         method: 'GET',
         url: '/'
       })
       .then(res => expect(res.response.config.headers.Authorization)
         .to.equal('Token 1234'));
+  });
+
+  it("should support passing of query params", () => {
+    return auth.request({
+        foo: 23,
+        bar: false,
+        conf: {baz: 'quux'}
+      }, {
+        method: 'GET',
+        url: '/'
+      })
+      .then(res => expect(res.response.config.params).to.deep.equal({
+        foo: 23,
+        bar: false
+      }));
   });
 });
